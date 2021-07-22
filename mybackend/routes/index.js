e9c3a6965f5ea9d83c22b93daff474dfca5d7e48
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2021-07-21 22:36:53 -0500

    initial

--- mybackend/routes/index.js
+++ mybackend/routes/index.js
@@ -1,12 +1,9 @@
-"use strict";
-/* eslint-disable */
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.send("wel Come To Crypto Payment Processer :)");
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
