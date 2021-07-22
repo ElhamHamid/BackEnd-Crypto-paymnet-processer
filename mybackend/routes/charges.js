fe0c174e3b0cca3858ce7815394e40e0da6cf6aa
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2021-07-21 23:18:24 -0500

    updating charges

--- mybackend/routes/charges.js
+++ mybackend/routes/charges.js
@@ -7,21 +7,24 @@ var router = express.Router();
 
 // creating charges
 router.post('/', async (req, res) =>{
+    let data={
+        "name": "The Sovereign Individual",
+        "description": "Mastering the Transition to the Information Age",
+        "local_price": {
+          "amount": req.body.price,
+          "currency": "USD"
+        },
+        "pricing_type": "fixed_price",
+      }
  try {
-   const resp= await axios.post("posts",{
-    "Content-Type": "application/json",
+   const resp= await 	 axios.post('https://api.commerce.coinbase.com/charges',{"body":data},{
+    headers: {
+     "Content-Type": "application/json",
     "X-CC-Api-Key": "377db512-71d1-48df-94fb-bbc62ee483d8" ,
     "X-CC-Version": "2018-03-22" ,
-   body:{
-     "name": "The Sovereign Individual",
-     "description": "Mastering the Transition to the Information Age",
-     "local_price": {
-       "amount": req.body.price,
-       "currency": "USD"
-     },
-     "pricing_type": "fixed_price",
-   }
-   })
+    }
+  }
+)
    res.status(201).json({status:"success",resp:resp.data})
  }catch {
     res.status(500).json({status:"faild, error occured"})
