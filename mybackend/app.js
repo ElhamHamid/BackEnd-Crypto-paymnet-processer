138cd9b5ec26475cdd8b1cf1cfde656382eabca7
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2021-07-26 11:09:49 -0500

    hosting front_end on the server

--- mybackend/app.js
+++ mybackend/app.js
@@ -3,12 +3,14 @@
 
 var express = require('express');
 var path = require('path');
+var fs=require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var chargesRouter=require('./routes/charges');
+const { dirname } = require('path');
 
 var app = express();
 
@@ -17,10 +19,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public\crypto-payment-processer')));
 app.use(cors());
-app.use('/', indexRouter);
+app.use('/', (req,res)=>{
+  fs.createReadStream(dirname + 'public/crypto-payment-processer/index.html').pipe(res);
+});
 app.use('/charges',chargesRouter);
+app.use('*',(req,res)=>{
+  fs.createReadStream(dirname + 'public/crypto-payment-processer/index.html').pipe(res);
+})
 
 // error handler
 app.use(function(err, req, res, next) {
