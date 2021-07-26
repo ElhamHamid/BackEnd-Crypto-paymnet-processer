f77e11d75ba171a49f9cb53c9fbaad4dddd24bca
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2021-07-26 11:21:51 -0500

    updated11

--- mybackend/app.js
+++ mybackend/app.js
@@ -1,39 +1,43 @@
 "use strict";
 /* eslint-disable */
 
-var express = require('express');
-var path = require('path');
-var fs=require('fs');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var chargesRouter=require('./routes/charges');
-const { dirname } = require('path');
+var indexRouter = require("./routes/index");
+var chargesRouter = require("./routes/charges");
 
 var app = express();
 
 // view engine setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public\crypto-payment-processer')));
+app.use(
+  express.static(path.join(__dirname, "public/crypto-payment-processer"))
+);
 app.use(cors());
-app.use('/', (req,res)=>{
-  fs.createReadStream(__dirname + 'public/crypto-payment-processer/index.html').pipe(res);
+app.use("/", (req, res) => {
+  fs.createReadStream(
+    path.join(__dirname, "public/crypto-payment-processer/index.html")
+  ).pipe(res);
 });
-app.use('/charges',chargesRouter);
-app.use('*',(req,res)=>{
-  fs.createReadStream(__dirname + 'public/crypto-payment-processer/index.html').pipe(res);
-})
+app.use("/charges", chargesRouter);
+app.use("*",(req, res) => {
+  fs.createReadStream(
+    path.join(__dirname, "public/crypto-payment-processer/index.html")
+  ).pipe(res);
+});
 
 // error handler
-app.use(function(err, req, res, next) {
-  res.status(500).json({err:"serverside Error"});
-
+app.use(function (err, req, res, next) {
+  res.status(500).json({ err: "serverside Error" });
 });
 
 // module.exports = app;
-app.listen(process.env.PORT || 7000)
+app.listen(process.env.PORT || 7000);
